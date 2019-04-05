a4e62711708380b3ef858d84e1db17e4c7d1ea8e
Richard Long <RLong@milestraining.lab>
2019-04-05 16:18:50 -0400

    created the event seeder file.

--- seeders/20190405185627-event.js
+++ seeders/20190405185627-event.js
@@ -12,6 +12,26 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+   return queryInterface.bulkInsert('Events',[{
+     name: 'NodeJs Walk Threw',
+     createdUserId: 1,
+     location: '120 braithwaite lane',
+     startingDayAndTime: '2019-05-05 09:00:00',
+     endingDayAndTime: '2019-05-05 11:00:00',
+     description: 'learn node with model and migrations and seeding',
+     createdAt: new Date(),
+     updatedAt: new Date()
+   },
+   {
+    name: 'Angular 7.0',
+    createdUserId: 1,
+    location: 'dairy queen',
+    startingDayAndTime: '2019-04-24 10:00:00',
+    endingDayAndTime: '2019-04-24 11:00:00',
+    description: 'Learn one way to build applications with Angular and reuse your code and abilities to build apps for any deployment target. For web, mobile web, native mobile and native desktop',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +42,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
+   return queryInterface.bulkDelete('Events', null, {});
   }
 };
