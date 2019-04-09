acd838935f3be2e6cd679bcca86e8308f0c4b8b5
Richard Long <RLong@milestraining.lab>
2019-04-09 01:27:40 -0400

    create is now working from postman

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -3,40 +3,35 @@ const validator = require('validator');
 
 const create = async function (req, res) {
   res.setHeader('ContentType', 'application/json');
-  const body = req.body;
+  let err, event, eventInfo;
+  eventInfo = req.body;
 
-  if (!body.name) {
+  if (!eventInfo.name) {
     return ReE(res, 'Please enter an name of Event', 422);
-  } else if (!body.location) {
-    return ReE(res, 'Please enter the location of event', 422);
+  } else if (!eventInfo.location) {
+    return ReE(res, 'Please enter the location of Event', 422);
   } else {
-    let err, event
-
-    [err, event] = await to(events.save());
-    if (err) return ReE(res, err, 422);
+    [err, event] = await to(Events.create(eventInfo));
+    if (err) {
+      return ReE(res, err, 422);
+    }
+      return res.json({ success: false, error: err });
+    }
+  
+    [err, event] = await to(Events.save());
+    if (err) {
+      return ReE(res, err, 422);
+    }  
     return ReS(res, event, 201);
-  }
 }
 module.exports.create = create;
-
-const createEvent = async function (eventInfo) {
-  let err;
-  if (validator.isEmail(eventInfo.email)) {
-    [err, event] = await to(Events.create(eventInfo));
-    if (err) TE('event already exists with that email');
-    return event;
-  } else {
-    TE('Email is invalid');
-  }
-}
-module.exports.createEvent = createEvent;
 
 const update = async function (req, res) {
   let err, event, data;
   event = req.event;
   data = req.body;
   event.set(data);  //sequelize set command 
-  [err, event] = await to(event.save());     //sequelize save() 
+  [err, event] = await to(Events.save());     //sequelize save() 
   if (err) {
     if (typeof err == 'object' && typeof err.message != 'undefined') {
       err = err.message;
